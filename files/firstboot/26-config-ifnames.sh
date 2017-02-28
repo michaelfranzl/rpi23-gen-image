702905ca07fad77d8bf24e76b2a958ec69955a1f
Michael Franzl <office@michaelfranzl.com>
2017-02-28 11:00:45 +0100

    fix 26-config-ifnames script

--- files/firstboot/26-config-ifnames.sh
+++ files/firstboot/26-config-ifnames.sh
@@ -2,7 +2,7 @@ logger -t "rc.firstboot" "Configuring network interface name"
 
 INTERFACE_NAME=$(dmesg | grep "renamed from eth0" | awk -F ":| " '{ print $9 }')
 
-if [ ! -z INTERFACE_NAME ] ; then
+if [ ! -z $INTERFACE_NAME ] ; then
   if [ -r "/etc/systemd/network/eth.network" ] ; then
     sed -i "s/eth0/${INTERFACE_NAME}/" /etc/systemd/network/eth.network
   fi
